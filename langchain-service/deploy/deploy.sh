a9db63a471e58381c6338587cbd5e0d29ad30a07
Aditya Narayan Mishra <mishraaditya384@outlook.com>
2023-07-02 20:37:09 +0530

    py verson change

--- langchain-service/deploy/deploy.sh
+++ langchain-service/deploy/deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 pwd
+python --version
 python3 -m pip install --upgrade pip
+python3 -m pip install pyenv
+# Set the Python version you want to install
+PYTHON_VERSION="3.11.0"  # Update this to the version you want
+
+# Install the specified Python version using pyenv
+pyenv install $PYTHON_VERSION
+
+# Set the installed version as the default one
+pyenv global $PYTHON_VERSION
+
+# Verify the new Python version
+python --version
+
 pip install -r requirements.txt
