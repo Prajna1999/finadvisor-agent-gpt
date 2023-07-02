b2ca5480775e68ede61a6c71a12695d31dce2372
Aditya Narayan Mishra <mishraaditya384@outlook.com>
2023-07-02 20:47:49 +0530

    py

--- langchain-service/deploy/deploy.sh
+++ langchain-service/deploy/deploy.sh
@@ -3,16 +3,4 @@ pwd
 python --version
 python3 -m pip install --upgrade pip
 
-cat /etc/*-release
-
-sudo apt-get update
-sudo apt-get install software-properties-common
-
-# Add deadsnakes PPA
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-
-# Install the Python version you want
-sudo apt-get install python3.11
-
 pip install -r requirements.txt
